5cfd2bf3666baff06f8acd3a23031f4218c84e25
micael1ma <micael1ma.professional@gmail.com>
2025-04-14 22:58:17 -0300

    WebSocket

--- src/index.js
+++ src/index.js
@@ -1,21 +1,53 @@
-let express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const authRouter = require("./routes/authRouter");
-const pizzaRouter = require("./routes/pizzaRouter");
-const orderRouter = require("./routes/orderRouter");
+let express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const authRouter = require('./routes/authRouter');
+const pizzaRouter = require('./routes/pizzaRouter');
+const orderRouter = require('./routes/orderRouter');
 
-const db = require("./database/db");
+const db = require('./database/db');
+
+const { createServer } = require('http');
+const { WebSocketServer, WebSocket } = require('ws');
 
 const app = express();
+const server = createServer(app);
+const ws = new WebSocketServer({ server });
+
+const clients = new Set();
+
+ws.on('connection', (client) => {
+  clients.add(client);
+
+  client.on('message', (message) => {
+    try {
+      const parsedMessage = JSON.parse(message);
+      console.log('Raw data received:', parsedMessage, 'Type:', typeof parsedMessage);
+
+      for (let c of clients) {
+        if (c.readyState === WebSocket.OPEN) {
+          c.send(JSON.stringify(parsedMessage));
+        }
+      }
+    } catch (error) {
+      console.error('Error parsing:', error);
+      client.send(JSON.stringify({ error: 'Invalid JSON format' }));
+    }
+  });
+
+  client.on('close', () => {
+    clients.delete(client);
+  });
+});
+
 app.use(cors());
 
 app.use(bodyParser.json());
-app.use("/api", authRouter);
-app.use("/api", pizzaRouter);
-app.use("/api", orderRouter);
+app.use('/api', authRouter);
+app.use('/api', pizzaRouter);
+app.use('/api', orderRouter);
 
 const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+server.listen(port, () => {
+  console.log(`Server with WebSocket is running on port ${port}`);
 });
