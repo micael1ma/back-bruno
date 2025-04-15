fb2f87148de38f84e6bd43329db92bce27344d7e
lucaslarry <lucasslarryy@gmail.com>
2025-04-15 17:45:33 -0300

    ia

--- src/index.js
+++ src/index.js
@@ -1,58 +1,39 @@
-let express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const authRouter = require('./routes/authRouter');
-const pizzaRouter = require('./routes/pizzaRouter');
-const orderRouter = require('./routes/orderRouter');
-const aiRouter = require('./routes/aiRouter');
+let express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const authRouter = require("./routes/authRouter");
+const pizzaRouter = require("./routes/pizzaRouter");
+const orderRouter = require("./routes/orderRouter");
+const aiRouter = require("./routes/aiRouter");
 
-const db = require('./database/db');
+const db = require("./database/db");
 
-const { createServer } = require('http');
-const { WebSocketServer, WebSocket } = require('ws');
+const { createServer } = require("http");
+const { WebSocketServer, WebSocket } = require("ws");
+const aiService = require("./services/AiService");
 
 const app = express();
 const server = createServer(app);
 const ws = new WebSocketServer({ server });
-const clients = new Set();
 
-ws.on('connection', (client) => {
-  clients.add(client);
-  console.log('Cliente adicionado');
+ws.on("connection", (client) => {
+  console.log("Cliente adicionado");
 
-  client.on('message', (message) => {
-    try {
-      const parsedMessage = JSON.parse(message);
-      console.log(
-        'Raw data received:',
-        parsedMessage,
-        'Type:',
-        typeof parsedMessage,
-      );
-
-      for (let c of clients) {
-        if (c.readyState === WebSocket.OPEN) {
-          c.send(JSON.stringify(parsedMessage));
-        }
-      }
-    } catch (error) {
-      console.error('Error parsing:', error);
-      client.send(JSON.stringify({ error: 'Invalid JSON format' }));
-    }
-  });
-
-  client.on('close', () => {
-    clients.delete(client);
+  client.on("message", async (message) => {
+    const msg = message.toString();
+    const msgJson = JSON.parse(msg);
+    const result = await aiService.longContext(msgJson.question);
+    client.send(JSON.stringify({ text: result.text(), sentBy: "Gemini" }));
   });
 });
 
 app.use(cors());
 
 app.use(bodyParser.json());
-app.use('/api', authRouter);
-app.use('/api', pizzaRouter);
-app.use('/api', orderRouter);
-app.use('/api', aiRouter);
+app.use("/api", authRouter);
+app.use("/api", pizzaRouter);
+app.use("/api", orderRouter);
+app.use("/api", aiRouter);
 
 const port = 3000;
 server.listen(port, () => {
